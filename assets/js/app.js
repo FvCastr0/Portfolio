7f17336f22aa8cc26e1f45ac1262572534345469
Fernando <101474411+FvCastr0@users.noreply.github.com>
2023-09-03 09:43:00 -0300

    About Me

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,10 @@
-window.sr = ScrollReveal({ reset: true })
-
+window.sr = ScrollReveal({ reset: false })
 document.addEventListener('DOMContentLoaded', () => {
     new TypeIt(".animated", {
-        speed: 70,
+        speed: 50,
     }).go()
 })
-sr.reveal('.title', { delay: 2000 })
-sr.reveal('.subtitle', { delay: 2300 })
-sr.reveal('.btnAbout', { delay: 2450 })
-sr.reveal('.icons', { delay: 2600 })
+sr.reveal('.title', { delay: 1500 })
+sr.reveal('.subtitle', { delay: 1700 })
+sr.reveal('.btnAbout', { delay: 2000 })
+sr.reveal('.icons', { delay: 2300 })
