63e44e517856ff2f6a0d2c55df00e1422378f21d
Fernando <101474411+FvCastr0@users.noreply.github.com>
2023-09-08 02:21:07 -0300

    Final version and responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,4 +7,3 @@ document.addEventListener('DOMContentLoaded', () => {
 sr.reveal('.title', { delay: 1500 })
 sr.reveal('.subtitle', { delay: 1700 })
 sr.reveal('.btnAbout', { delay: 2000 })
-sr.reveal('.icons', { delay: 2300 })
